044ef3607cf99ce753d2db3fdca401cd304042da
David Songer <cdsonger@gmail.com>
2011-11-14 14:11:08 -0500

    Base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Team04::Application.config.secret_token = 'da79f8c6bc5f50875fbd06986a54df0f180c55142415047d232637173a8eb9c530688b7b2341adb294b89d60fa5ab9ab6aeaccdbdb2c963e07589e5f08ba13a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Team04::Application.config.session_store :cookie_store, key: '_team_04_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Team04::Application.config.session_store :active_record_store
