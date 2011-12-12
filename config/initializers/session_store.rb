088a6f0fd4a5378b10f18d6254ea42cd4bf033fd
Faiz Abbasi <faizabbasi@faiz-abbasis-macbook.local>
2011-12-11 20:56:17 -0500

    prep for merge

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Team04::Application.config.session_store :cookie_store, key: '_team_04_session'
+Team04::Application.config.session_store :cookie_store, :key => '_team_04_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
